2c583801a5ac8eedda1c83452f9a0a127123c7f4
Earle Fox <earle.fox312@gmail.com>
2023-01-09 11:06:01 -0500

    updated name variable

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// This is a comment
+Console.WriteLine("I am acitvie");
+
+//example for string to number
+Console.WriteLine("Please enter a radius");
+//sring result = Console.ReadLine();
+//double num = double.Parse(result); -- this lines are combined into one line below 
+double num = double.Parse(Console.ReadLine());
+double area = num * num * Math.PI;
+Console.WriteLine(area);
+//NULL example 
+//NULL set a variable value as a placeholder
+string myName = null;
+Console.WriteLine(myName);
+string friend = "Josh Carolin";
+
+if (friend == "Family")
+{
+    myName = "Justin Jones";
+}
+else if (friend == "Josh Carolin")
+{
+    myName = "Jeo";
+}
+Console.WriteLine(myName);
